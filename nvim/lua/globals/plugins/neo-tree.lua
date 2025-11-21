316f58524c84afc217e1b7e98c8aeec3059baa62
Dheebz <148539956+Dheebz@users.noreply.github.com>
2025-11-21 23:43:41 +1100

    conf(nvim): neo-tree better exclusions

--- nvim/lua/globals/plugins/neo-tree.lua
+++ nvim/lua/globals/plugins/neo-tree.lua
@@ -26,6 +26,26 @@ return {
         enabled = true,
       },
       hijack_netrw_behavior = "open_default",
+
+      -- 🔽 NEW: filtering behaviour
+      filtered_items = {
+        -- when false: hidden items are not shown at all (you can toggle them with H in neo-tree)
+        visible = false,
+
+        -- show dotfiles like .gitignore, .env, etc.
+        hide_dotfiles = false,
+
+        -- hide things that are in .gitignore
+        hide_gitignored = true,
+
+        -- specifically hide .gitkeep
+        hide_by_name = {
+          ".gitkeep",
+        },
+
+        -- optional, but you can also hard-hide other junk here if you want:
+        -- never_show = { ".DS_Store", "thumbs.db" },
+      },
     },
     window = {
       width = 32,
