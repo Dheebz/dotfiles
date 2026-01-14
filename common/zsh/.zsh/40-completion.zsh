d66377a044ddc6fe01765ec8f3993283e3d6c74e
Dheebz <148539956+Dheebz@users.noreply.github.com>
2026-01-14 23:20:37 +1100

    update

--- common/zsh/.zsh/40-completion.zsh
+++ common/zsh/.zsh/40-completion.zsh
@@ -1,8 +1,13 @@
 # ~/.zsh/40-completion.zsh
 # Completion system and related enhancements
 
+# spotify-cli completions
+fpath=(~/.zsh/completions $fpath)
+export PATH="$HOME/.local/bin:$PATH"
+
 autoload -Uz compinit && compinit
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # gh completions
 if command -v gh >/dev/null 2>&1; then
--- common/zsh/.zshrc
+++ common/zsh/.zshrc
@@ -1,12 +1,3 @@
 for file in ~/.zsh/*.zsh; do
 	source "$file"
 done
-
-# ==========================
-# ==== Edit Below this Line
-# ==========================
-
-# spotify-cli completions
-fpath=(~/.zsh/completions $fpath)
-autoload -Uz compinit && compinit
-export PATH="$HOME/.local/bin:$PATH"
